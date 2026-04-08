18bd31906404189cf954023486f67d8ddbc135e3
DevZenthor <zenthorpro@outlook.com>
2026-04-08 20:00:46 +0200

    fix ; fix bug telephone

--- nitsy_portfolio/src/components/Navbar.jsx
+++ nitsy_portfolio/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../styles/components/Navbar.css";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,23 +17,27 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className={`navbar navbar-expand-lg fixed-top ${scrolled ? "navbar-scrolled" : "navbar-top"}`}>
-      <div className="container">
+    <nav className={`navbar ${scrolled ? "navbar-scrolled" : "navbar-top"}`}>
+      <div className="container nav-container">
 
         {/* LOGO */}
-        <Link className="navbar-brand logo" to="/">
+        <Link to="/" className="logo">
           Nitsy
         </Link>
 
+        {/* BURGER ICON */}
+        <div className="burger" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
         {/* MENU */}
-        <div className="ms-auto d-flex align-items-center">
+        <div className={`nav-menu ${menuOpen ? "active" : ""}`}>
 
-          <Link className="nav-link" to="/">Accueil</Link>
-          <Link className="nav-link" to="/portfolio">Portfolio</Link>
-          <Link className="nav-link" to="/clients">Clients</Link>
-          <Link className="nav-link" to="/services">Services</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Accueil</Link>
+          <Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link>
+          <Link to="/clients" onClick={() => setMenuOpen(false)}>Clients</Link>
+          <Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link>
 
-          {/* CONTACT = DISCORD */}
           <a
             href="https://discord.gg/VepYaxpraG"
             target="_blank"
